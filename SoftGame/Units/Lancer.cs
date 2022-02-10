b048c60e62884535a4c14f081fc8cd36510a8684
Kvitka Mykola <mykola.kvitka@hneu.net>
2022-02-10 16:42:02 +0200

    Implement Lancer.cs

---
+++ SoftGame/Units/Lancer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SoftGame.Skills.Interfaces;
+
+namespace SoftGame.Units
+{
+    public class Lancer : Warrior
+    {
+        public int AttackToSecoundTarget = 0;
+
+        private int _reduceDamage = 2;
+        
+        public Lancer()
+        {
+            Health = 50;
+            Attack = 6;
+        }
+
+        public override bool AttackTarget(IUnit warrior)
+        {
+           var healthBeforeAttack =  warrior.Health;
+                
+            base.AttackTarget(warrior);
+
+            if (warrior.IsAlive)
+            {
+                AttackToSecoundTarget = healthBeforeAttack - warrior.Health;
+            }
+            else
+            {
+                AttackToSecoundTarget = healthBeforeAttack + Math.Abs(warrior.Health);
+            }
+
+            AttackToSecoundTarget /= _reduceDamage;
+            
+            return warrior.IsAlive;
+        }
+    }
+}
